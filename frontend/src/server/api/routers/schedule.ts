5cb76fe1f317a491e937386d61e49328697a36be
Rishi Kumar <rishi.kumar.2837@gmail.com>
2025-02-08 15:19:16 -0500

    Added get_schedule

--- frontend/src/server/api/routers/schedule.ts
+++ frontend/src/server/api/routers/schedule.ts
@@ -16,4 +16,22 @@ export const scheduleRouter = createTRPCRouter({
             await ctx.client.post(`/users/${input.userId}/schedule/ics`, input.ics);
         }),
 
+    get_schedule: protectedProcedure
+        .input(z.object({
+            userId: z.number()
+        }))
+        .query(async ({ ctx, input }) => {
+            const res = await ctx.client.get(`/users/${input.userId}/schedule`);
+
+            const schema = z.object({
+                name: z.string(),
+                days: z.string().array(),
+                start: z.string(),
+                end: z.string()
+            });
+
+            const result = await schema.safeParseAsync(res.data);
+
+            if (result.success) return result.data;
+        }),
 });
